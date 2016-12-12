2bf872e56d754df5576de1781633747bc51fb928
liuderchi <liuderchi@gmail.com>
2016-12-12 18:11:35 +0800

    :art: POC fetching content from github
    - add command line argv in script/build
    - remove localization/*

--- script/lib/i18n.js
+++ script/lib/i18n.js
@@ -1,34 +1,71 @@
 'use strict'
-/*
-STEPS to localize Atom in build process
-
-git clone && cd atom
-git checkout v1.x.x
-./script/build   # NOTE should build first then localize
-node i18n.js -l "zh-tw"
-./script/build --create-debian-package
-sudo dpkg -i out/atom-amd64.deb
-*/
 
 const fs = require('fs')
 const CSON = require('season')
-const WORKDIR = "out/app/node_modules"
+const path = require('path')
+const syncRequest = require('sync-request')
 const CONFIG = require('../config')
 
-const dependentPackages = [
-  "tree-view"
-  // TODO get all core pkg requiring localization
-];
+const LocaleSupport = new Set([
+  "zh-tw"
+  // TODO add support
+])
+const packageDependencies = [
+  'autoflow',
+  'bookmarks',
+  'bracket-matcher',
+  'command-palette',
+  'dev-live-reload',
+  'encoding-selector',
+  'find-and-replace',
+  'fuzzy-finder',
+  'git-diff',
+  'go-to-line',
+  'grammar-selector',
+  'image-view',
+  'keybinding-resolver',
+  'markdown-preview',
+  'open-on-github',
+  'package-generator',
+  'settings-view',
+  'snippets',
+  'spell-check',
+  'styleguide',
+  'symbols-view',
+  'tabs',
+  'timecop',
+  'tree-view',
+  'welcome',
+  'whitespace'
+]
 
-module.exports = function() {
-  console.log(`Localizing JSON paths in ${CONFIG.intermediateAppPath}`)
-  for (let pkgName of dependentPackages) {
-    var targetFilePath = WORKDIR + "/" + pkgName + "/" + "menus/" + pkgName + ".json"
-    // TODO path util
+const getCsonSync = function(pkgName, locale) {
+  const url = `https://raw.githubusercontent.com/liuderchi/atom-i18n-cson/master/${locale}/${pkgName}.cson`
+  const resp = syncRequest('GET', url)
+  if (resp.statusCode === 200) {
+    return CSON.parse(resp.getBody('utf8'))
+  }
+  return false
+}
 
-    var local = undefined || 'zh-tw';  // TODO Parse argument for local
-    var sourceFilePath = 'localization/' + local + '/' + pkgName + '.cson'
+module.exports = function(locale) {
+  locale = locale.toLowerCase()
+  if (locale === undefined) {
+    console.log('Skipping localization'.gray)
+    return
+  }
+  if (!LocaleSupport.has(locale)) {
+    console.error(`locale ${locale} not supported!`.red)
+    process.exit(1)
+  }
+  console.log(`Localizing JSON paths in ${CONFIG.intermediateAppPath} with locale ${locale}`)
 
-    fs.writeFileSync(targetFilePath, JSON.stringify(CSON.readFileSync(sourceFilePath)))
+  for (let pkgName of packageDependencies) {
+    const cson = getCsonSync(pkgName, locale)
+    if (cson) {
+      const targetFilePath = path.join(CONFIG.intermediateAppPath, "node_modules", pkgName, "menus", pkgName + ".json")
+      console.log(`i18n: updating files: ${targetFilePath}`.blue)
+      fs.writeFileSync(targetFilePath, JSON.stringify(cson))
+    }
   }
 }
